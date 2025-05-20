5bc2f749f06562d838fc8185480b1e5b2d14c52d
mateo-rfz <mahdifeyzolahy@gmail.com>
2025-05-20 11:29:04 +0330

    add noobi arg parser and add filter for test(shell and min uid)

--- src/whoimnot.cpp
+++ src/whoimnot.cpp
@@ -8,6 +8,23 @@ using namespace std;
 
 
 
+
+
+string 
+noobiArgParser (int argc , char * argv[] , string target)
+{
+    for (int i = 0 ; i < argc ; i++)
+    {
+        if (argv[i] == target)
+        {
+            return argv[i+1];
+        }
+    }
+    return "";
+}
+
+
+
 int
 main(int argc , char * argv[])
 {
@@ -15,9 +32,46 @@ main(int argc , char * argv[])
     static int UID = getuid();
     passwd * pswd;
 
+
+
+    //filter by min uid
+    string arg = noobiArgParser(argc , argv , "--uid-min");
+    if (arg != "")
+    {
+        while ((pswd = getpwent()) != 0)
+    {
+        if (pswd->pw_uid != UID && pswd->pw_uid > stoi(arg))
+        {
+            cout << pswd->pw_name << endl;
+        }
+    }
+    }
+
+
+
+    //filter by shell
+    arg = noobiArgParser(argc , argv , "--shell");
+    if (arg != "")
+    {
+        while ((pswd = getpwent()) != 0)
+    {
+        if (pswd->pw_uid != UID && pswd->pw_shell == arg)
+        {
+            cout << pswd->pw_name << endl;
+        }
+    }
+    }
+
+
+
+
+
+
+
+
     while ((pswd = getpwent()) != 0)
     {
-        if (pswd->pw_uid != UID && pswd-> pw_uid > 999 )
+        if (pswd->pw_uid != UID)
         {
             cout << pswd->pw_name << endl;
         }
